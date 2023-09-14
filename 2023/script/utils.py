499f4745f28e1dcdacc746f49d970d88ce1e1bf3
chihduo <chihduo.hong@gmail.com>
2023-09-14 18:32:28 +0800

    Update utils.py

--- 2023/script/utils.py
+++ 2023/script/utils.py
@@ -19,20 +19,16 @@ def convert_list_to_event_log(list, log):
 def draw_dfg(log):
   dfg, ia, fa = pm4py.discover_dfg(log)
   pm4py.view_dfg(dfg, ia, fa)
-  return dfg
 
 def draw_proc_tree(log):
   tree = pm4py.discovery.discover_process_tree_inductive(log)
   pm4py.vis.view_process_tree(tree)
-  return tree
 
 def draw_heur_net(log):
   net = pm4py.discover_heuristics_net(log)
   pm4py.view_heuristics_net(net)
-  return net
 
 def draw_perf_dfg(log, aggr="mean"):
   dfg, ia, fa = pm4py.discovery.discover_performance_dfg(log)
   pm4py.vis.view_performance_dfg(dfg, ia, fa, aggregation_measure=aggr)
-  return dfg
   
